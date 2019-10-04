c288271a2f12e5ce6fe706c681f585f74e69bfcd
Shikhar Saxena <shikharsaxena8@gmail.com>
2019-10-05 03:07:34 +0530

    minor change

--- get.js
+++ get.js
@@ -2,7 +2,7 @@
   const url = window.location.href;
   chrome.storage.local.get("scroll-mark", data => {
     const scrollMarkData = data["scroll-mark"];
-    const scrollMarkOffset = scrollMarkData[url].offset;
+    const scrollMarkOffset = scrollMarkData[url].offset || data["scroll-mark"];
     window.scrollTo({ left: 0, top: scrollMarkOffset, behavior: "smooth" });
   });
 }
