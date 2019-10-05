791d247af5349fbf7506c5d2596df6b94945e051
Fabeha Rizvi <fabrizvi786@gmail.com>
2019-10-05 16:14:37 +0530

    Option Page styled

--- options.js
+++ options.js
@@ -1,12 +1,13 @@
 {
-  const list = document.getElementById("saved-urls");
-  var urls = "";
-  chrome.storage.local.get("scroll-mark", function(result) {
-    urls = result["scroll-mark"];
-    for (var url in urls) {
-      var div = document.createElement("div");
-      div.innerHTML = "<a href=" + url + ">" + url + "</a>";
-      list.appendChild(div);
-    }
-  });
+	const list = document.getElementById("saved-urls");
+	var urls = "";
+	var favicon = "/images/favicon-16x16.png";
+	chrome.storage.local.get("scroll-mark", function(result) {
+		urls = result["scroll-mark"];
+		for (var url in urls) {
+			var div = document.createElement("div");
+			div.innerHTML = `<a href=${url}><div class='bookmark'><img src=${favicon} alt='bookmark-logo'><p>${url}</p></div></a>`;
+			list.appendChild(div);
+		}
+	});
 }
