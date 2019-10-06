e0eb3dfd3ad9e0a532218b16f27a3173e50f0d42
Shikhar Saxena <shikharsaxena8@gmail.com>
2019-10-06 13:37:49 +0530

    Added Page title in Chrome storage and options page

--- save.js
+++ save.js
@@ -4,9 +4,11 @@
     const scrollMarkData = data["scroll-mark"];
     const offset = window.pageYOffset;
     const total = document.body.clientHeight;
+    const title = document.title;
+    console.log(document.title);
     const newData = scrollMarkData
-      ? { ...scrollMarkData, [url]: { offset, total } }
-      : { [url]: { offset, total } };
+      ? { ...scrollMarkData, [url]: { offset, total, title } }
+      : { [url]: { offset, total, title } };
     console.log(offset, scrollMarkData[url], newData[url]);
     chrome.storage.local.set({ "scroll-mark": newData }, () => {
       chrome.runtime.sendMessage("setActive");
