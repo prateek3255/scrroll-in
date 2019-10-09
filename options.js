aa1d0b81495670741fa654982092161873ef823b
Fabeha Rizvi <fabrizvi786@gmail.com>
2019-10-09 07:15:04 +0530

    Bookmark in header removed and edited option.js file for 0% #9 #15

--- options.js
+++ options.js
@@ -10,7 +10,8 @@
 			let percentage = Math.round((urls[url].offset / urls[url].total) * 100);
 			if (percentage)
 				div.innerHTML = `<a href=${url}><div class='bookmark'><img src=${icon} alt='bookmark-logo'><p>${title}</p><span>${percentage}%</span></div></a>`;
-			else div.innerHTML = "<a href=" + url + ">" + title + "</a>";
+			else
+				div.innerHTML = `<a href=${url}><div class='bookmark'><img src=${icon} alt='bookmark-logo'><p>${title}</p><span>${percentage}%</span></div></a>`;
 			list.appendChild(div);
 		}
 	});
