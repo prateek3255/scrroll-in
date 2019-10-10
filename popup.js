3a3739cf4f4a945a12f90ea0fa41f4e17b794b1a
Fabeha Rizvi <fabrizvi786@gmail.com>
2019-10-11 02:15:10 +0530

    Changes made in Options and Popup css styles as required in #35 with changes in Popup.js file

--- popup.js
+++ popup.js
@@ -1,52 +1,54 @@
-const root = document.getElementById("root");
-root.innerHTML = "<div> Loading...</div>";
+// const root = document.getElementById("root");
+const popup = document.querySelector(".pop-opt");
+const p = document.querySelector(".p");
+const green = document.querySelector(".green-btn");
+const buttons = document.querySelector(".buttons");
+const image = document.querySelector(".image");
+// popup.innerHTML = "<div> Loading...</div>";
 
-chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-  const url = tabs[0].url;
-  chrome.storage.local.get("scroll-mark", data => {
-    const scrollMarkData = data["scroll-mark"];
-    if (scrollMarkData && scrollMarkData.hasOwnProperty(url)) {
-      root.innerHTML = `
-      <div style="margin:15px;">
-      <div style="margin-bottom:10px;display:flex;justify-content:center;width:100%;">
-      <button id="getScroll" style="width:100%;" class="btn">Fetch Scroll</button>
-      </div>
-      <div style="display:flex; width:200px;">
-      <button class="btn orange" style="width:100%;margin-right:10px" id="saveScroll">Update</button>
-      <button class="btn red" style="width:100%;" id="deleteScroll">Delete</button>
-      </div>
-      <div>
-      `;
-      let deleteScroll = document.getElementById("deleteScroll");
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+	const url = tabs[0].url;
+	chrome.storage.local.get("scroll-mark", (data) => {
+		const scrollMarkData = data["scroll-mark"];
+		if (scrollMarkData && scrollMarkData.hasOwnProperty(url)) {
+			popup.height = "278px";
+			image.src = "/images/icon-256.png";
+			p.textContent = `Continue from where you last left or update/delete scrroll for this page.`;
+			green.innerHTML = `<button id="getScroll" class="btn-font green">Fetch Scrroll</button>`;
+			buttons.innerHTML = `<button class="btns" id="saveScroll">Update</button><button class="btns red" id="deleteScroll">Delete</button>`;
 
-      deleteScroll.onclick = function(element) {
-        chrome.tabs.executeScript(tabs[0].id, {
-          file: "delete.js",
-          allFrames: true
-        });
-        window.close();
-      };
+			let deleteScroll = document.getElementById("deleteScroll");
 
-      let getScroll = document.getElementById("getScroll");
+			deleteScroll.onclick = function(element) {
+				chrome.tabs.executeScript(tabs[0].id, {
+					file: "delete.js",
+					allFrames: true
+				});
+				window.close();
+			};
 
-      getScroll.onclick = function(element) {
-        chrome.tabs.executeScript(tabs[0].id, {
-          file: "get.js",
-          allFrames: true
-        });
-        window.close();
-      };
-    } else {
-      root.innerHTML = `<button style="width:100px; margin:15px" class="btn" id="saveScroll">Save</button>`;
-    }
-    let saveScroll = document.getElementById("saveScroll");
+			let getScroll = document.getElementById("getScroll");
 
-    saveScroll.onclick = function(element) {
-      chrome.tabs.executeScript(tabs[0].id, {
-        file: "save.js",
-        allFrames: true
-      });
-      window.close();
-    };
-  });
+			getScroll.onclick = function(element) {
+				chrome.tabs.executeScript(tabs[0].id, {
+					file: "get.js",
+					allFrames: true
+				});
+				window.close();
+			};
+		} else {
+			popup.height = "224px";
+			p.textContent = `In a hurry! Save the scrroll and read this page at your own pace by clicking the button below 👇`;
+			green.innerHTML = `<button id="saveScroll" class=" btn-font green">Save</button>`;
+		}
+		let saveScroll = document.getElementById("saveScroll");
+
+		saveScroll.onclick = function(element) {
+			chrome.tabs.executeScript(tabs[0].id, {
+				file: "save.js",
+				allFrames: true
+			});
+			window.close();
+		};
+	});
 });
