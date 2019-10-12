a7605d16e33124fd123dec861c7740beab6bcd77
RohitKaushal7 <rohitkk252550@gmail.com>
2019-10-12 17:18:41 +0530

    options page- grid view

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 {
   const list = document.getElementById("saved-urls");
   var urls = "";
-  chrome.storage.local.get("scroll-mark", function(result) {
+  chrome.storage.local.get("scroll-mark", function (result) {
     urls = result["scroll-mark"];
     for (var url in urls) {
       let title = urls[url].title || url;
@@ -9,7 +9,7 @@
       let percentage = Math.round((urls[url].offset / urls[url].total) * 100);
       if (percentage)
         div.innerHTML =
-          "<a href=" + url + ">" + title + "</a> " + percentage + "%";
+          "<a href=" + url + ">" + title + "</a> " + "<div class='perc'>" + percentage + "</div>";
       else div.innerHTML = "<a href=" + url + ">" + title + "</a>";
       list.appendChild(div);
     }
