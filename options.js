dfcbdafc83be3d9c2ee25f3f61089b1f9d23ea0f
devcer <santosh.viswanatham@levadata.com>
2019-10-17 13:57:36 +0530

    fix: issue #50

--- options.js
+++ options.js
@@ -1,8 +1,12 @@
 {
   const list = document.getElementById("saved-urls");
+  const clearAllBtn = document.getElementById('clear-all-btn');
   var urls = "";
   chrome.storage.local.get("scroll-mark", function (result) {
     urls = result["scroll-mark"];
+    if(Object.keys(urls).length > 0) {
+      clearAllBtn.style.display = 'inline-block';
+    }
     for (var url in urls) {
       let title = urls[url].title || url;
       var div = document.createElement("div");
@@ -12,6 +16,14 @@
           "<a href=" + url + ">" + title + "</a> " + "<div class='perc'>" + percentage + "</div>";
       else div.innerHTML = "<a href=" + url + ">" + title + "</a>";
       list.appendChild(div);
+    }
+  });  
+  clearAllBtn.addEventListener('click', () => {
+    if(confirm("Are you sure you want to clear everything?")) {
+      chrome.storage.local.set({ "scroll-mark": {} }, (data) => {
+        list.innerHTML = "";
+        clearAllBtn.style.display = 'none';
+      });
     }
   });
 }
