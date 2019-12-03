3f7219f1d2ee2bde75879e291808a27277647e40
Prateek Surana <prateeksurana3255@gmail.com>
2019-12-04 01:23:16 +0530

    :recycle: Added minify css and html tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,61 @@
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const del = require('del');
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
+let cleanCSS = require("gulp-clean-css");
+const htmlmin = require("gulp-htmlmin");
+const del = require("del");
 
-gulp.task('clean', done => {
-    del.sync('build/**/*');
-    done()
-})
+gulp.task("clean", done => {
+  del.sync("build/**/*");
+  done();
+});
 
-gulp.task('scripts', done => {
-    gulp.src('*.js')
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/preset-env']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('build'))
-    done()
-})
+gulp.task("minify-scripts", done => {
+  gulp
+    .src(["*.js", "!gulpfile.js"])
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("build"));
+  done();
+});
 
-gulp.task('copy', done => {
-    gulp.src(['manifest.json', '*.html', '*.css', 'images/**/*', 'Roboto/**/*'], { base: "." })
-        .pipe(gulp.dest('build'));
-    done()
-})
+gulp.task("minify-css", done => {
+  gulp
+    .src("*.css")
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("build"));
+  done();
+});
 
-gulp.task('default', gulp.series('clean', 'scripts', 'copy', done => done()))
+gulp.task("minify-html", done => {
+  gulp
+    .src("*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+  done();
+});
+
+gulp.task("copy", done => {
+  gulp
+    .src(["manifest.json", "images/**/*", "Roboto/**/*", "utils/**/*"], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
+  done();
+});
+
+gulp.task(
+  "default",
+  gulp.series(
+    "clean",
+    "minify-scripts",
+    "minify-css",
+    "minify-html",
+    "copy",
+    done => done()
+  )
+);
