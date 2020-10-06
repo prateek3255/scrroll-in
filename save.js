d5a97bd1bee4f4fb251fa004c3ab4f47ea311280
Shikhar Saxena <shikharsaxena8@gmail.com>
2020-10-07 00:22:26 +0530

    Removed debugging statements

--- save.js
+++ save.js
@@ -11,23 +11,16 @@
     const date = String(new Date());
     let arr = [];
     if (scrollMarkData[url] && !scrollMarkData[url] instanceof Array) {
-      //If there is an object, convert it into array and push that old object as well as new object into the array.
 
+      //If there is an object, convert it into array and push that old object as well as new object into the array.
       arr.push({ ...scrollMarkData[url], id: 0 });
       arr.push({ id, offset, total, title, date });
-      console.log(1);
-      console.log(arr);
     } else {
       arr = scrollMarkData[url] ? scrollMarkData[url] : [];
       arr.push({ id, offset, total, title, date });
-      console.log(2);
-      console.log(arr);
     }
     console.log(arr);
     const newData = scrollMarkData ? { ...scrollMarkData, [url]: arr } : { [url]: arr };
-
-    console.log("newdata");
-    console.log(newData);
     chrome.storage.local.set({ "scroll-mark": newData }, () => {
       chrome.runtime.sendMessage("setActive");
     });
