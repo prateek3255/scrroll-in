2ab55624a710149792bc6a6f1d83d5e04efcd06c
Shikhar Saxena <shikharsaxena8@gmail.com>
2020-10-07 00:21:17 +0530

    Resolved bugs in previous commit

--- get.js
+++ get.js
@@ -4,10 +4,10 @@
 
   chrome.storage.local.get("scroll-mark", data => {
     const scrollMarkData = data["scroll-mark"];
-
+    console.log(scrollMarkData);
     if (scrollMarkData[url]) {
       const scrollMarkOffset =
-        scrollMarkData[url].offset || scrollMarkData[url][0].offset || scrollMarkData;
+        scrollMarkData[url].offset || scrollMarkData[url][0]?.offset || scrollMarkData;
       window.scrollTo({ left: 0, top: scrollMarkOffset, behavior: "smooth" });
     }
   });
--- save.js
+++ save.js
@@ -4,22 +4,30 @@
 
   chrome.storage.local.get("scroll-mark", data => {
     const scrollMarkData = data["scroll-mark"];
-    const id = scrollMarkData[url].length ? scrollMarkData.length : (scrollMarkData ? 1:0);
+    const id = scrollMarkData[url]?.length ? scrollMarkData.length : (scrollMarkData ? 1 : 0);
     const offset = window.pageYOffset;
     const total = document.body.scrollHeight;
     const title = document.title;
     const date = String(new Date());
+    let arr = [];
     if (scrollMarkData[url] && !scrollMarkData[url] instanceof Array) {
-      //If there is an object, convert it into array and push that old as well as new object into the array.
-      const arr = [];
-      arr.push({...scrollMarkData[url],id:0});
-      arr.push({ id,offset, total, title, date });
-    } else {
-      const arr = scrollMarkData[url] ? scrollMarkData[url] : [];
-      arr.push({ id,offset, total, title, date });
-    }
-    const newData = { ...scrollMarkData, [url]: arr }
+      //If there is an object, convert it into array and push that old object as well as new object into the array.
 
+      arr.push({ ...scrollMarkData[url], id: 0 });
+      arr.push({ id, offset, total, title, date });
+      console.log(1);
+      console.log(arr);
+    } else {
+      arr = scrollMarkData[url] ? scrollMarkData[url] : [];
+      arr.push({ id, offset, total, title, date });
+      console.log(2);
+      console.log(arr);
+    }
+    console.log(arr);
+    const newData = scrollMarkData ? { ...scrollMarkData, [url]: arr } : { [url]: arr };
+
+    console.log("newdata");
+    console.log(newData);
     chrome.storage.local.set({ "scroll-mark": newData }, () => {
       chrome.runtime.sendMessage("setActive");
     });
