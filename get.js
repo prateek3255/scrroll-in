4ff140a76d040cefcdb79d1391fc25ab7b8c55c5
Shikhar Saxena <shikhar.saxena@comviva.com>
2020-10-06 15:57:08 +0530

    Changed Data Structure to allow multiple pins

--- get.js
+++ get.js
@@ -4,9 +4,10 @@
 
   chrome.storage.local.get("scroll-mark", data => {
     const scrollMarkData = data["scroll-mark"];
+
     if (scrollMarkData[url]) {
       const scrollMarkOffset =
-        scrollMarkData[url].offset || scrollMarkData[url];
+        scrollMarkData[url].offset || scrollMarkData[url][0].offset || scrollMarkData;
       window.scrollTo({ left: 0, top: scrollMarkOffset, behavior: "smooth" });
     }
   });
--- save.js
+++ save.js
@@ -4,13 +4,22 @@
 
   chrome.storage.local.get("scroll-mark", data => {
     const scrollMarkData = data["scroll-mark"];
+    const id = scrollMarkData[url].length ? scrollMarkData.length : (scrollMarkData ? 1:0);
     const offset = window.pageYOffset;
     const total = document.body.scrollHeight;
     const title = document.title;
-    let date = String(new Date());
-    const newData = scrollMarkData
-      ? { ...scrollMarkData, [url]: { offset, total, title, date } }
-      : { [url]: { offset, total, title, date } };
+    const date = String(new Date());
+    if (scrollMarkData[url] && !scrollMarkData[url] instanceof Array) {
+      //If there is an object, convert it into array and push that old as well as new object into the array.
+      const arr = [];
+      arr.push({...scrollMarkData[url],id:0});
+      arr.push({ id,offset, total, title, date });
+    } else {
+      const arr = scrollMarkData[url] ? scrollMarkData[url] : [];
+      arr.push({ id,offset, total, title, date });
+    }
+    const newData = { ...scrollMarkData, [url]: arr }
+
     chrome.storage.local.set({ "scroll-mark": newData }, () => {
       chrome.runtime.sendMessage("setActive");
     });
