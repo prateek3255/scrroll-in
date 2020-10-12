ca699be8fe0873899c0e9d708b3059361db458fe
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-10-12 13:16:59 +0530

    Added sort by date functionality

--- options.js
+++ options.js
@@ -20,8 +20,10 @@
         <div class="btn del" id="delete-all"> <img src='./images/bin.png'> </div>
       </div>
       `;
+      const btn2 = document.createElement("button");
+      btn2.innerHTML = "<button class='btn primary' id='sort-button'>Sort by Date </button>";
       document.body.appendChild(btn);
-
+      document.body.appendChild(btn2);
       for (var url in urls) {
         const title = urls[url].title || url;
         const url_id = url;
@@ -102,7 +104,7 @@
 
     const scrollElements = document.getElementsByClassName("ScrollElement");
     const deleteAllBtn = document.getElementById("delete-all");
-
+    const sortbutton = document.getElementById('sort-button');
     // TODO: use the delete.js script instead
     // but that would require some modifications to the original delete functionality
     const deleteScrollElement = function (element) {
@@ -143,12 +145,140 @@
         }
       });
     };
+    const sortbydate = function (e) {
+      //console.log("list=", list);
+
+      while (list.firstChild) {
+        list.removeChild(list.firstChild);
+      }
+      //console.log("list=", list);
+      //alert("sort by date");
+      var obj = [];
+      i = 0;
+      for (var url in urls) {
+        //console.log(url);
+        obj[i] = urls[url];
+        obj[i].url = url;
+        i = i + 1;
+        //console.log(urls[url]);
+      }
+
+      obj.sort(function (a, b) {
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(a.date) - new Date(b.date);
+      });
+      //console.log("obj", obj);
+      var listUrls = [];
+      //console.log(listUrls);
+
+      //const urls2 = new Object();
+      for (var i1 = 0; i1 < obj.length;) {
+        listUrls[i1] = obj[i1].url;
+        i1 = i1 + 1;
+        //console.log("urls=", obj[i].url);
+        //urls2[obj[i].url] = obj[i];
+      }
+      //console.log(listUrls);
+
+      for (var i = 0; i < listUrls.length; i++) {
+        const url = listUrls[i];
+        const title = urls[url].title || url;
+        const url_id = url;
+        const div = document.createElement("div");
+        div.setAttribute("class", "Scroll");
+        div.setAttribute(
+          "data-date",
+          urls[url].date ? urls[url].date.slice(0, 15) : "Date-Here"
+        );
+        const percentage = Math.round(
+          (urls[url].offset / urls[url].total) * 100
+        );
+
+        const delete_html1 = `
+        <div class="dropdown" id="${url_id}_|_">
+          <div id="${url_id}_|" class="dropdown-content">
+                <button class="ScrollElement" id="${url_id}">Delete</button>
+          </div>
+        </div>
+        `;
+
+        if (percentage)
+          div.innerHTML =
+            "<a href=" +
+            url +
+            ">" +
+            title +
+            "</a> " +
+            "<div class='perc'>" +
+            percentage +
+            "</div>" +
+            delete_html1;
+        else
+          div.innerHTML =
+            "<a href=" +
+            url +
+            ">" +
+            title +
+            "</a> " +
+            "<div class='perc'>" +
+            0 +
+            "</div>" +
+            delete_html1;
+        div.style.animationDelay = (delay += 0.1) + "s";
+        list.appendChild(div);
+      }
+      //console.log("finallist", list);
+      const showDropdown1 = function (e) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+          dropdowns[i].classList.remove("show_dropdown");
+        }
+        const drop_id = this.id.substring(0, this.id.length - 1);
+        document.getElementById(drop_id).classList.toggle("show_dropdown");
+      };
+
+      // function to close dropdown menu
+      const closeDropdown1 = function (e) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        if (e.target.className !== "dropdown") {
+          for (let i = 0; i < dropdowns.length; i++) {
+            dropdowns[i].classList.remove("show_dropdown");
+          }
+        }
+      };
+      const dotElements1 = document.getElementsByClassName("dropdown");
+
+      for (let i = 0; i < dotElements1.length; i++) {
+        dotElements1[i].addEventListener("click", showDropdown1, false);
+      }
+
+      const containers1 = document.getElementsByClassName("container");
+      containers1[0].addEventListener("click", closeDropdown1);
+
+      const scrollElements1 = document.getElementsByClassName("ScrollElement");
+      const deleteAllBtn1 = document.getElementById("delete-all");
+      const sortbutton = document.getElementById('sort-button');
+
+      for (let i = 0; i < scrollElements1.length; i++) {
+        scrollElements1[i].addEventListener("click", deleteScrollElement, false);
+      }
+      if (deleteAllBtn1) {
+        deleteAllBtn1.addEventListener("click", deleteAllScrollElement, false);
+      }
+      if (sortbutton) {
+        sortbutton.addEventListener("click", sortbydate, false);
+      }
+    }
 
     for (let i = 0; i < scrollElements.length; i++) {
       scrollElements[i].addEventListener("click", deleteScrollElement, false);
     }
     if (deleteAllBtn) {
       deleteAllBtn.addEventListener("click", deleteAllScrollElement, false);
+    }
+    if (sortbutton) {
+      sortbutton.addEventListener("click", sortbydate, false);
     }
   });
 }
