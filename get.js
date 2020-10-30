c821df8cf6e3067531b9cffd54724a717a5e01ac
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-10-30 13:24:24 +0530

    Added Context Saving functionality to recollect

--- get.js
+++ get.js
@@ -8,6 +8,9 @@
       const scrollMarkOffset =
         scrollMarkData[url].offset || scrollMarkData[url];
       window.scrollTo({ left: 0, top: scrollMarkOffset, behavior: "smooth" });
+      if (document.readyState === "complete" && scrollMarkData[url].note !== undefined && scrollMarkData[url].note !== null) {
+        alert("We hope you recollect from here :)\n" + scrollMarkData[url].note);
+      }
     }
   });
 }
--- save.js
+++ save.js
@@ -1,7 +1,7 @@
 {
   const fullUrl = window.location.href;
   const url = fullUrl.split("?")[0];
-
+  var note = prompt("Enter the context:");
   chrome.storage.local.get("scroll-mark", data => {
     const scrollMarkData = data["scroll-mark"];
     const offset = window.pageYOffset;
@@ -9,8 +9,8 @@
     const title = document.title;
     let date = String(new Date());
     const newData = scrollMarkData
-      ? { ...scrollMarkData, [url]: { offset, total, title, date } }
-      : { [url]: { offset, total, title, date } };
+      ? { ...scrollMarkData, [url]: { offset, total, title, date, note } }
+      : { [url]: { offset, total, title, date, note } };
     chrome.storage.local.set({ "scroll-mark": newData }, () => {
       chrome.runtime.sendMessage("setActive");
     });
